1e4e8834dfe9c039cdb5a4f0c92ad05f0593aa6a
arieditya <arieditya.prdh@yahoo.com>
2015-02-05 17:16:48 +0700

    admin teacher driven class

---
+++ application/views/admin/teacher_driven/class_list.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by :
+ * 
+ * User: AndrewMalachel
+ * Date: 2/5/15
+ * Time: 4:00 PM
+ * Proj: prod-new
+ */?><!DOCTYPE html>
+<html>
+<head>
+	
+</head>
+<!-- Insert Initial/Loader JS here -->
+<body>
+</body>
+<!-- Insert JS here -->
+</html>
