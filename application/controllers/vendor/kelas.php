c1199e28d1bce0130d01cc738a2510b65283c1f5
arieditya <arieditya.prdh@yahoo.com>
2015-02-05 17:29:39 +0700

    vendor list kelas

--- application/controllers/vendor/kelas.php
+++ application/controllers/vendor/kelas.php
@@ -20,7 +20,8 @@ class Kelas extends Vendor_Controller{
 	}
 	
 	public function daftar() {
-		$list_class = $this->vendor_class_model->get_class(array('vendor_id'=>$this->data['user']['id'],'class_status'=>NULL, 'active'=>NULL))->result();
+		$list_class = $this->vendor_class_model->get_class(array('vendor_id'=>$this->data['user']['id'],
+																 'class_status >='=>NULL, 'active'=>NULL))->result();
 		foreach($list_class as &$list) {
 			$list->level = $this->vendor_class_model->get_class_level($list->id);
 			$list->category = $this->vendor_class_model->get_class_category($list->id);
