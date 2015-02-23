984ee2f0d45b1367683cbe3a7c5bd36fd4b6a6d5
arieditya <arieditya.prdh@yahoo.com>
2015-02-23 15:21:57 +0700

    gtm GTM-N7V538

--- application/views/header.php
+++ application/views/header.php
@@ -1,5 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-		"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
 
 <head>
@@ -18,15 +17,14 @@
 	<script type="text/javascript" src="<?php echo base_url();?>js/bantuan.js"></script>
 </head>
 <body>
-
 <!-- Google Tag Manager -->
-<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-5RGCZZ"
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-N7V538"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-5RGCZZ');</script>
+})(window,document,'script','dataLayer','GTM-N7V538');</script>
 <!-- End Google Tag Manager -->
 
 	<script type="text/javascript">
