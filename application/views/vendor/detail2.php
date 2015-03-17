54270d7650d6a2c4fcc53dacc027331147f30586
arieditya <arieditya.prdh@yahoo.com>
2015-03-17 20:24:18 +0700

    new detail

---
+++ application/views/vendor/detail2.php
@@ -0,0 +1,284 @@
+<?php
+/**
+ * Created by :
+ * 
+ * User: AndrewMalachel
+ * Date: 10/14/14
+ * Time: 7:52 AM
+ * Proj: private-development
+ */
+$this->load->view('vendor/general/header2');
+?>
+	<div class="container content vendor-detail">
+		<div class="row">
+			<div class="col-md-8">
+				<div class="panel panel-default">
+					<div class="panel-heading heading-label">Sirclo</div>
+					<div class="panel-body">
+						<div class="col-md-4 img-profile">
+							<img src="assets/images/user.png" alt="" class="img-responsive">
+						</div>
+						<div class="col-md-8 profile-desc">
+							<p>SIRCLO adalah sebuah perusahaan teknologi yang menyediakan platform untuk pembuatan e-commerce. memiliki visi untuk membantu para wirausahawan yang membutuhkan. Di kelas ini kamu akan belajar tentang seluk beluk e-commerce, mendengarkan cerita dari para pelaku usaha online, hingga belajar membuat website sendiri. Untuk mengikuti kelas. Di kelas ini kamu akan belajar tentang seluk beluk e-commerce, mendengarkan cerita dari para pelaku usaha online, hingga belajar membuat website sendiri. Untuk mengikuti kelas.</p>
+							<p>Di kelas ini kamu akan belajar tentang seluk beluk e-commerce, mendengarkan cerita dari para pelaku usaha online, hingga belajar membuat website sendiri. Untuk mengikuti kelas. Di kelas ini kamu akan belajar tentang seluk beluk e-commerce, mendengarkan cerita dari para pelaku usaha online, hingga belajar membuat website sendiri. Untuk mengikuti kelas.</p>
+						</div>
+					</div>
+				</div><!-- hero-detail -->
+				<div class="review-wrap">
+					<div class="rating-wrap review-item">
+						<h4 class="review-title">Rating</h4>
+						<div class="rating pull-left">
+							<span class="rating-info"> <i class="fa fa-star"></i><b>4.5</b> dari <b>10</b> review</span>
+						</div>
+					</div><!-- rating-wrap -->
+					<div class="testimonial-wrap review-item">
+						<h4 class="review-title">Testimonial</h4>
+						<div class="testimonial-item">
+							<div class="icon-wrap">
+								<img src="assets/images/user.png" alt="" class="img-responsive">
+							</div>
+							<h5 class="username"><strong>Badrudin Al Fakher</strong> | Mengikuti kelas Parkour</h5>
+							<p>Kelasnya bagus banget! Saya belajar banyak mengenai tips dan trik bikin toko online. Sekarang toko saya jadi ramai dikunjungi pelanggan. Terima kasih!</p>
+						</div>
+						<div class="testimonial-item">
+							<div class="icon-wrap">
+								<img src="assets/images/user.png" alt="" class="img-responsive">
+							</div>
+							<h5 class="username"><strong>Badrudin Al Fakher</strong> | Mengikuti kelas Parkour</h5>
+							<p>Kelasnya bagus banget! Saya belajar banyak mengenai tips dan trik bikin toko online. Sekarang toko saya jadi ramai dikunjungi pelanggan. Terima kasih!</p>
+						</div>
+						<ul class="pagination pull-right">
+							<li>
+								<a href="#" aria-label="Previous">
+									<span aria-hidden="true">&laquo;</span>
+								</a>
+							</li>
+							<li><a href="#">1</a></li>
+							<li><a href="#">2</a></li>
+							<li><a href="#">3</a></li>
+							<li><a href="#">4</a></li>
+							<li><a href="#">5</a></li>
+							<li>
+								<a href="#" aria-label="Next">
+									<span aria-hidden="true">&raquo;</span>
+								</a>
+							</li>
+						</ul>
+					</div><!-- testimonial-wrap -->
+				</div><!-- review-wrap -->    
+			</div><!-- col-md-8 -->
+			<div class="col-md-4">
+				<div class="panel panel-default">
+					<div class="panel-heading heading-label text-center"><i class="fa fa-camera"></i> Foto</div>
+					<div class="panel-body">
+						<div class="thumb-wrap">
+							<ul>
+								<li><img src="assets/images/obama.jpg" class="img-responsive"></li>
+								<li><img src="assets/images/obama.jpg" class="img-responsive"></li>
+								<li><img src="assets/images/obama.jpg" class="img-responsive"></li>
+								<li><img src="assets/images/obama.jpg" class="img-responsive"></li>
+							</ul>
+						</div>
+						<div class="preview">
+							<img src="assets/images/obama.jpg" class="img-responsive">
+						</div>
+					</div>
+				</div><!-- panel -->
+				<div class="panel panel-default">
+					<div class="panel-heading heading-label text-center"><i class="fa fa-map-marker"></i> Kontak</div>
+					<div class="panel-body">
+						<p><i class="fa fa-map-marker"></i>  Jalan Tebet Raya 32 A Jakarta Selatan, Indonesia 12820</p> 
+						<h5 class="support"><a href="#"><i class="fa fa-phone-square"></i>021-9200-3040</a></h5>
+						<h5 class="support"><a href="#"><i class="fa fa-envelope"></i>info@sirclo.com</a></h5>
+						<h5 class="support"><a href="#"><i class="fa fa-facebook"></i>/sirclo</a></h5>
+						<h5 class="support"><a href="#"><i class="fa fa-twitter"></i>@sirclo</a></h5>
+
+					</div>
+				</div><!-- panel -->
+				<div class="panel panel-default">
+					<div class="panel-heading heading-label text-center"><i class="fa fa-question-circle"></i> Pertanyaan</div>
+					<div class="panel-body">
+						<p>Peroleh informasi dan bantuan terkait kelas dari tim layanan konsumen kami! </p>
+						<h5 class="support"><a href="#"><i class="fa fa-phone-square"></i>021-9200-3040</a></h5>
+						<h5 class="support"><a href="#"><i class="fa fa-envelope"></i>kelas@ruangguru.com</a></h5>
+					</div>
+				</div><!-- panel -->
+			</div><!-- col-md-4 -->
+			<div class="col-sm-12">
+				<div class="related-wrap">
+					<h2 class="block-title text-uppercase">Kelas yang diadakan Sirclo</h2>
+					<div class="row">
+						<div class="col-md-4 col-sm-6">
+							<div class="content-grid diskon">
+								<a href="#">
+									<div class="grid-top" style="background-image: url('assets/images/main_picture.jpg');">
+										<div class="grid-title-wrap">
+											<h3 class="grid-title">Training CodeIgniter: Breaking The Frame[works]</h3>
+										</div><!-- grid-title-wrap -->
+										<div class="diskon-label">
+											<h3>ADA</h3>
+											<h5>DISKON</h5>
+										</div>
+									</div><!-- grid-top -->  
+								</a>
+								<div class="grid-bottom">
+									<span class="price">Rp 75,000,- /sesi</span>
+									<a href="#"><span class="details">Daftar</span></a>
+									<div class="description">
+										<div class="icon"><i class="fa fa-calendar-o"></i></div>
+										<span class="date"> 08 Mar 2015 | 17.00 - 19.00 WIB <br> dan <a href="#">5 sesi lainnya</a> | Jakarta Selatan</span>
+									</div><!-- description -->
+									<div class="review">
+										<div class="icon"><i class="fa fa-shopping-cart"></i></div>
+										<span class="tag">kelas ruangguru</span>
+										<div class="rating">
+											<span class="rating-info"> <i class="fa fa-star"></i><b>4.5</b> dari <b>10</b> review</span>
+										</div>
+									</div>
+								</div><!-- grid-bottom -->
+							</div> <!-- content-grid -->
+						</div><!-- col-md-4 col-sm-6 -->
+						<div class="col-md-4 col-sm-6">
+							<div class="content-grid">
+								<a href="#">
+									<div class="grid-top" style="background-image: url('assets/images/main_picture.jpg');">
+										<div class="grid-title-wrap">
+											<h3 class="grid-title">Training CodeIgniter: Breaking The Frame[works]</h3>
+										</div><!-- grid-title-wrap -->
+									</div><!-- grid-top -->  
+								</a>
+								<div class="grid-bottom">
+									<span class="price">Rp 75,000,- /sesi</span>
+									<a href="#"><span class="details">Daftar</span></a>
+									<div class="description">
+										<div class="icon"><i class="fa fa-calendar-o"></i></div>
+										<span class="date"> 08 Mar 2015 | 17.00 - 19.00 WIB <br> dan <a href="#">5 sesi lainnya</a> | Jakarta Selatan</span>
+									</div><!-- description -->
+									<div class="review">
+										<div class="icon"><i class="fa fa-shopping-cart"></i></div>
+										<span class="tag">kelas ruangguru</span>
+										<div class="rating">
+											<span class="rating-info"> <i class="fa fa-star"></i><b>4.5</b> dari <b>10</b> review</span>
+										</div>
+									</div>
+								</div><!-- grid-bottom -->
+							</div> <!-- content-grid -->
+						</div><!-- col-md-4 col-sm-6 -->
+						<div class="col-md-4 col-sm-6">
+							<div class="content-grid">
+								<a href="#">
+									<div class="grid-top" style="background-image: url('assets/images/main_picture.jpg');">
+										<div class="grid-title-wrap">
+											<h3 class="grid-title">Training CodeIgniter: Breaking The Frame[works]</h3>
+										</div><!-- grid-title-wrap -->
+									</div><!-- grid-top -->  
+								</a>
+								<div class="grid-bottom">
+									<span class="price">Rp 75,000,- /sesi</span>
+									<a href="#"><span class="details">Daftar</span></a>
+									<div class="description">
+										<div class="icon"><i class="fa fa-calendar-o"></i></div>
+										<span class="date"> 08 Mar 2015 | 17.00 - 19.00 WIB <br> dan <a href="#">5 sesi lainnya</a> | Jakarta Selatan</span>
+									</div><!-- description -->
+									<div class="review">
+										<div class="icon"><i class="fa fa-shopping-cart"></i></div>
+										<span class="tag">kelas ruangguru</span>
+										<div class="rating">
+											<span class="rating-info"> <i class="fa fa-star"></i><b>4.5</b> dari <b>10</b> review</span>
+										</div>
+									</div>
+								</div><!-- grid-bottom -->
+							</div> <!-- content-grid -->
+						</div><!-- col-md-4 col-sm-6 -->
+						<div class="col-md-4 col-sm-6">
+							<div class="content-grid diskon">
+								<a href="#">
+									<div class="grid-top" style="background-image: url('assets/images/main_picture.jpg');">
+										<div class="grid-title-wrap">
+											<h3 class="grid-title">Training CodeIgniter: Breaking The Frame[works]</h3>
+										</div><!-- grid-title-wrap -->
+										<div class="diskon-label">
+											<h3>ADA</h3>
+											<h5>DISKON</h5>
+										</div>
+									</div><!-- grid-top -->  
+								</a>
+								<div class="grid-bottom">
+									<span class="price">Rp 75,000,- /sesi</span>
+									<a href="#"><span class="details">Daftar</span></a>
+									<div class="description">
+										<div class="icon"><i class="fa fa-calendar-o"></i></div>
+										<span class="date"> 08 Mar 2015 | 17.00 - 19.00 WIB <br> dan <a href="#">5 sesi lainnya</a> | Jakarta Selatan</span>
+									</div><!-- description -->
+									<div class="review">
+										<div class="icon"><i class="fa fa-shopping-cart"></i></div>
+										<span class="tag">kelas ruangguru</span>
+										<div class="rating">
+											<span class="rating-info"> <i class="fa fa-star"></i><b>4.5</b> dari <b>10</b> review</span>
+										</div>
+									</div>
+								</div><!-- grid-bottom -->
+							</div> <!-- content-grid -->
+						</div><!-- col-md-4 col-sm-6 -->
+						<div class="col-md-4 col-sm-6">
+							<div class="content-grid">
+								<a href="#">
+									<div class="grid-top" style="background-image: url('assets/images/main_picture.jpg');">
+										<div class="grid-title-wrap">
+											<h3 class="grid-title">Training CodeIgniter: Breaking The Frame[works]</h3>
+										</div><!-- grid-title-wrap -->
+									</div><!-- grid-top -->  
+								</a>
+								<div class="grid-bottom">
+									<span class="price">Rp 75,000,- /sesi</span>
+									<a href="#"><span class="details">Daftar</span></a>
+									<div class="description">
+										<div class="icon"><i class="fa fa-calendar-o"></i></div>
+										<span class="date"> 08 Mar 2015 | 17.00 - 19.00 WIB <br> dan <a href="#">5 sesi lainnya</a> | Jakarta Selatan</span>
+									</div><!-- description -->
+									<div class="review">
+										<div class="icon"><i class="fa fa-shopping-cart"></i></div>
+										<span class="tag">kelas ruangguru</span>
+										<div class="rating">
+											<span class="rating-info"> <i class="fa fa-star"></i><b>4.5</b> dari <b>10</b> review</span>
+										</div>
+									</div>
+								</div><!-- grid-bottom -->
+							</div> <!-- content-grid -->
+						</div><!-- col-md-4 col-sm-6 -->
+						<div class="col-md-4 col-sm-6">
+							<div class="content-grid">
+								<a href="#">
+									<div class="grid-top" style="background-image: url('assets/images/main_picture.jpg');">
+										<div class="grid-title-wrap">
+											<h3 class="grid-title">Training CodeIgniter: Breaking The Frame[works]</h3>
+										</div><!-- grid-title-wrap -->
+									</div><!-- grid-top -->  
+								</a>
+								<div class="grid-bottom">
+									<span class="price">Rp 75,000,- /sesi</span>
+									<a href="#"><span class="details">Daftar</span></a>
+									<div class="description">
+										<div class="icon"><i class="fa fa-calendar-o"></i></div>
+										<span class="date"> 08 Mar 2015 | 17.00 - 19.00 WIB <br> dan <a href="#">5 sesi lainnya</a> | Jakarta Selatan</span>
+									</div><!-- description -->
+									<div class="review">
+										<div class="icon"><i class="fa fa-shopping-cart"></i></div>
+										<span class="tag">kelas ruangguru</span>
+										<div class="rating">
+											<span class="rating-info"> <i class="fa fa-star"></i><b>4.5</b> dari <b>10</b> review</span>
+										</div>
+									</div>
+								</div><!-- grid-bottom -->
+							</div> <!-- content-grid -->
+						</div><!-- col-md-4 col-sm-6 -->
+						<div class="col-sm-4 col-sm-offset-4">
+							<a href="#" class="main-button text-center">Lihat Kelas Lainnya</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div><!-- row -->
+	</div> <!-- /container -->
+<?php
+$this->load->view('vendor/general/footer2');
