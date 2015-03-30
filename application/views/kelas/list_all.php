3ecddc6abb742a73515aa48a5ff5f684fb678a34
tsaqibb <saqib.abud@gmail.com>
2015-03-30 09:32:57 +0700

    list all

--- application/views/kelas/list_all.php
+++ application/views/kelas/list_all.php
@@ -129,7 +129,7 @@ $this->load->view('vendor/general/header2');
                         </div><!-- description -->
                         <div class="review">
                             <div class="vendor-name">
-                                <div class="icon tag"><i class="fa fa-shopping-cart"></i></div>
+                                <div class="icon tag"><i class="fa fa-user"></i></div>
                                 <a href="#"><?php echo character_limiter($kelas->vendor['profile']->name,12);?></a>
                             </div>
                             <div class="rating">
